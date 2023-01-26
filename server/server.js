6a912b873833cf6180f8e7aea58a0d601615f5be
chrisbrights <softtalents1986@outlook.com>
2023-01-27 02:54:31 +0900

    merge server and client by one package.json file

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.SERVER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
